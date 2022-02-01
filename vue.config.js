a1304eda5a1e4b99da4d4b2e22ddd0d2ac9f7007
won <2won4won@gmail.com>
2022-02-01 10:59:27 +0900

    vue.config.js 3

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ?'https://2won4won.github.io/Wonhago/'
-      :'/',
+  outputDir: '/docs',
+  publicpath: '/portfolio',
   css : {
     loaderOptions : {
       sass : {         
