3c50ff8d585e3ec4a168dd80e4418c842b538e09
won <2won4won@gmail.com>
2022-02-16 00:46:05 +0900

    size 조정

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,26 @@
 <template>
   <div id="app">
-     <router-view/>
+     <Header />
+     <!-- <router-view></router-view> -->
+     <!-- <Footer /> -->
   </div>
 </template>
 <script>
+import Header from '@/components/Header';
+
 export default {
   name: "app",
   components: {
+    Header
   }
 }
 </script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Poppins, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: Poppins,antialiased;
+  -moz-osx-font-smoothing: Poppins,grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
-// #nav {
-//   padding: 30px;
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
 </style>
